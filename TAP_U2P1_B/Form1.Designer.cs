deabaf9f31a01fb751fa04dfc195035da831e3d9
salvaalejos <alejossalvador@gmail.com>
2024-10-13 15:54:29 -0600

    Tarea primeros hilos, funcion de minutos, s

---
+++ TAP_U2P1_B/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace TAP_U2P1_B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Salmon;
+            this.label1.Location = new System.Drawing.Point(240, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(314, 77);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 150;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(247, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(309, 77);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(358, 342);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hilos";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TAP_U2P1_B/Form1.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.VisualBasic.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TAP_U2P1_B
+{
+    public partial class Form1 : Form
+    {
+        private Thread thread;
+        private List<int> proyectos = new List<int>();
+        
+        private int intentos = 100;
+        private int tiempo = 0;
+        private int minutos = 0;
+
+        public Form1()
+        {
+            proyectos.Add(1);
+            proyectos.Add(2);
+            proyectos.Add(3);
+            proyectos.Add(4);
+            proyectos.Add(5);
+            proyectos.Add(6);
+            proyectos.Add(7);
+            proyectos.Add(8);
+            proyectos.Add(9);
+            proyectos.Add(10);
+            proyectos.Add(11);
+            proyectos.Add(12);
+            proyectos.Add(13);
+            proyectos.Add(14);
+            proyectos.Add(15);
+            thread = new Thread(hiloTipo1);
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            thread.Start();
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void hiloTipo1()
+        {
+            while(true)
+            {
+                Console.WriteLine("Soy el hilo tipo 1");
+                new Log().WriteEntry("Hilo tipo 1");
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            thread.Abort();
+            backgroundWorker1.CancelAsync();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int indice = random.Next(proyectos.Count);
+
+            label1.Text = "Proyecto: " + proyectos[indice];
+
+            intentos--;
+            if(intentos <= 0)
+            {
+                timer1.Stop();
+                proyectos.RemoveAt(indice);
+
+                label1.ForeColor = Color.Teal;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            intentos = 100;
+            label1.ForeColor = Color.Salmon;
+
+            timer1.Start();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while(true)
+            {
+                tiempo++;
+                if(tiempo == 60)
+                {
+                    tiempo = 0;
+                    minutos++;
+                }
+                backgroundWorker1.ReportProgress(1);
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (minutos == 0)
+                label2.Text = "Tiempo: " + tiempo + " segundos";
+            else if (tiempo == 0)
+                label2.Text = "Tiempo: " + minutos + " minutos";
+            else if ( minutos == 1 && tiempo == 1)
+                label2.Text = "Tiempo: " + minutos + " minuto, " + tiempo + " segundo";
+            else if ( minutos == 1 && tiempo > 1)
+                label2.Text = "Tiempo: " + minutos + " minuto, " + tiempo + " segundos";
+            else if ( minutos > 1 && tiempo == 1)
+                label2.Text = "Tiempo: " + minutos + " minutos, " + tiempo + " segundo";
+            else if ( minutos > 0 && tiempo > 0) 
+                label2.Text = "Tiempo: " + minutos + " minutos, " + tiempo + " segundos";
+            
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Console.WriteLine("Fin del hilo 3");
+        }
+    }
+}
